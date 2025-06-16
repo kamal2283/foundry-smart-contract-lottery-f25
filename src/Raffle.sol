d37c571623a6115db8e263c39cf0972213d2f113
Kamal Shah <shahkamal2283@gmail.com>
2025-06-17 02:17:49 +0530

    Created Raffle and Counter contracts and set up Foundry project

---
+++ src/Raffle.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle Contract
+ * @author Kamal Shah
+ * @notice This contract is for creatign a sampple raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    // Getter Functions
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
